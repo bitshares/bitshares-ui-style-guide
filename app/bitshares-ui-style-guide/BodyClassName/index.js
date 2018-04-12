af6a4a5e2ba7766f418c59d1e8fe0964114c4c6b
Leroy Jethro Gibbs <leroygibbsfromncis@gmail.com>
2018-04-12 15:28:01 +0200

    Add bodyClassName component to put theme name to body

---
+++ app/bitshares-ui-style-guide/BodyClassName/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class BodyClassName extends React.Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+  };
+
+  _getList(className) {
+    return className.length ? className.split(' ') : [];
+  }
+
+  componentDidMount() {
+    this._getList(this.props.className).forEach((className) => {
+      if (!document.body.classList.contains(className)) {
+        document.body.classList.add(className);
+      }
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+    const currentClassName = this._getList(this.props.className);
+    const nextClassName = this._getList(nextProps.className);
+
+
+    currentClassName.forEach((className) => {
+      if(nextClassName.indexOf(className) === -1)
+        document.body.classList.remove(className);
+    });
+
+    nextClassName.forEach((className) => {
+      if(!document.body.classList.contains(className)) {
+        document.body.classList.add(className);
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this._getList(this.props.className).forEach((className) => {
+      document.body.classList.remove(className);
+    });
+  }
+
+  render() {
+    return this.props.children;
+  }
+
+}
+
+export default BodyClassName;
