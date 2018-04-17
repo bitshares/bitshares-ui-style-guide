9fc7ec8a10464f289c833f1bb1bfd7f822e7bbdf
Leroy Jethro Gibbs <leroygibbsfromncis@gmail.com>
2018-04-18 01:53:47 +0200

    Light: Modal

--- app/components/Basic/modal.js
+++ app/components/Basic/modal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import bg from 'assets/images/bitshares-screenshot-bg.png';
+import lightBg from 'assets/images/bitshares-screenshot-bg-light.png';
 
 import {Modal as Mdl, BodyClassName} from 'bitshares-ui-style-guide';
 
@@ -13,9 +14,14 @@ class Modal extends React.Component {
       height: '150%',
     };
 
+    let logo = bg;
+
+    if(this.props.match.params.theme === 'lightTheme')
+      logo = lightBg;
+
     return (
       <BodyClassName className={this.props.match.params.theme}>
-        <img src={bg} style={style}/>
+        <img src={logo} style={style}/>
         <Mdl title="Modal Example"
              visible={true}>
           <p>The quick agile trader exchanged ZCASH for BTS</p>
