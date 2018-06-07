4b57f3d94982908a1ee1fcacb8cd4799d7c5c6a5
Leroy Jethro Gibbs <leroygibbsfromncis@gmail.com>
2018-06-07 04:19:51 +0200

    Fix demo page code to fix eslint warnings

--- app/bitshares-ui-style-guide/index.js
+++ app/bitshares-ui-style-guide/index.js
@@ -2,45 +2,45 @@ import "./dark.less";
 import "./light.less";
 import "./midnight.less";
 
-import {Row, Col, Form} from 'antd';
+import {Row, Col, Form} from "antd";
 
-import BodyClassName from './BodyClassName';
-import Button from './Button';
-import Input from './Input';
-import Select from './Select';
-import Upload from './Upload';
-import Checkbox from './Checkbox';
-import Radio from './Radio';
-import Switch from './Switch';
-import InputNumber from './InputNumber';
-import Table from './Table';
-import Tabs from './Tabs';
-import Slider from './Slider';
-import Breadcrumb from './Breadcrumb';
-import Modal from './Modal';
-import Layout from './Layout';
-import Icon from './Icon';
+import BodyClassName from "./BodyClassName";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import Upload from "./Upload";
+import Checkbox from "./Checkbox";
+import Radio from "./Radio";
+import Switch from "./Switch";
+import InputNumber from "./InputNumber";
+import Table from "./Table";
+import Tabs from "./Tabs";
+import Slider from "./Slider";
+import Breadcrumb from "./Breadcrumb";
+import Modal from "./Modal";
+import Layout from "./Layout";
+import Icon from "./Icon";
 
 export {
-  /* wrapped ANT components */
-  BodyClassName,
-  Button,
-  Input,
-  Slider,
-  Select,
-  Upload,
-  Checkbox,
-  InputNumber,
-  Breadcrumb,
-  Modal,
-  Layout,
-  Radio,
-  Switch,
-  Table,
-  Icon,
-  Tabs,
-  /* default ANT components */
-  Row,
-  Col,
-  Form
+    /* wrapped ANT components */
+    BodyClassName,
+    Button,
+    Input,
+    Slider,
+    Select,
+    Upload,
+    Checkbox,
+    InputNumber,
+    Breadcrumb,
+    Modal,
+    Layout,
+    Radio,
+    Switch,
+    Table,
+    Icon,
+    Tabs,
+    /* default ANT components */
+    Row,
+    Col,
+    Form
 };
