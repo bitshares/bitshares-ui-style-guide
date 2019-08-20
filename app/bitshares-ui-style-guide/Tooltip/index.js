43fe400feac7277d1fabf3a08c5327d9d29a5314
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2019-08-20 17:43:46 +0300

    #2809 Expand default mouseEnter delay to 0.5s

--- app/bitshares-ui-style-guide/Tooltip/index.js
+++ app/bitshares-ui-style-guide/Tooltip/index.js
@@ -1,3 +1,23 @@
-import {Tooltip} from "antd";
+import React, {Component} from "react";
+import {Tooltip as AntTooltip} from "antd";
+
+class Tooltip extends Component {
+
+    constructor(){
+        super();
+        this.defaultDelay = 0.5;
+    }
+
+    render() {
+        return (
+            <AntTooltip
+                mouseEnterDelay={this.defaultDelay}
+                {...this.props}
+            >
+                {this.props.children}
+            </AntTooltip>
+        );
+    }
+}
 
 export default Tooltip;
